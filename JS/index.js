0eb557aad56a7c581cda6abff63c5dc601b58fe0
JorgeDevOps-GitHub <jorgeDevOps20@gmail.com>
2021-07-03 15:25:50 -0500

    Menu Responsive

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,32 @@
 let bandera = true;
 function menu() {
   var element = document.getElementById("menu");
-  bandera = !bandera;
-  bandera ? element.classList.add("menusvisible")
-    : element.classList.remove("menusvisible");
+  console.log(bandera);
+
+  if (bandera == true) {
+    document.getElementById("menu");
+    element.classList.add("menuvisible");
+    bandera = false;
+  } else {
+    document.getElementById("menu");
+    element.classList.remove("menuvisible");
+    bandera = true;
+  }
 }
 function menu_movil() {
   var element = document.getElementById("menu-movil");
   var element1 = document.getElementById("btnMenu");
-  bandera = !bandera;
-  
-  bandera ? element.classList.add("menusvisible")
-    : element.classList.remove("menusvisible");
-  
-    bandera ? element1.classList.add("correrBtn")
-    : element1.classList.remove("correrBtn");
+  bandera = bandera
+  if (bandera == true) {
+    element.classList.add("menusvisible");
+    element1.classList.add("correrBtn");
+    
+    bandera = false;
+  } else {
+    element.classList.remove("menusvisible");
+    element1.classList.remove("correrBtn");
+
+    bandera = true;
+  }
 }
+
